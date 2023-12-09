accbd58f0e91fec3383d2950d038dae9959191ce
Amokke <24.02.1985@mail.ru>
2023-12-09 12:30:34 +0700

    add Readme.md

---
+++ sql/10.sql
@@ -0,0 +1,6 @@
+USE Human_friends;
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM camels;
+
+SELECT Name, Birthday, Commands FROM horses
+UNION SELECT  Name, Birthday, Commands FROM donkeys;
---
+++ sql/9.sql
@@ -0,0 +1,80 @@
+USE Human_friends;
+INSERT INTO cats (Name, Birthday, Commands, Genus_id)
+VALUES ('Барсик', '2012-02-02', 'кс-кс-кс', 1),
+('Мурзик', '2013-03-03', "отставить!", 1),  
+('Кузя', '2014-04-04', "", 1); 
+
+CREATE TABLE dogs 
+(       
+    Id INT AUTO_INCREMENT PRIMARY KEY, 
+    Name VARCHAR(20), 
+    Birthday DATE,
+    Commands VARCHAR(50),
+    Genus_id int,
+    Foreign KEY (Genus_id) REFERENCES home_animals (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO dogs (Name, Birthday, Commands, Genus_id)
+VALUES ('Дик', '2020-01-01', 'ко мне, лежать, лапу, голос', 2),
+('Граф', '2021-06-12', "сидеть, лежать, лапу", 2),  
+('Шарик', '2018-05-01', "сидеть, лежать, лапу, след, фас", 2), 
+('Босс', '2021-05-10', "сидеть, лежать, фу, место", 2);
+
+CREATE TABLE hamsters 
+(       
+    Id INT AUTO_INCREMENT PRIMARY KEY, 
+    Name VARCHAR(20), 
+    Birthday DATE,
+    Commands VARCHAR(50),
+    Genus_id int,
+    Foreign KEY (Genus_id) REFERENCES home_animals (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO hamsters (Name, Birthday, Commands, Genus_id)
+VALUES ('Хома', '2011-01-01', '', 3),
+('Пух', '2012-02-02', "атака сверху", 3),  
+('Серый', '2013-03-03', NULL, 3), 
+('Бур', '2014-04-04', NULL, 3);
+
+CREATE TABLE horses 
+(       
+    Id INT AUTO_INCREMENT PRIMARY KEY, 
+    Name VARCHAR(20), 
+    Birthday DATE,
+    Commands VARCHAR(50),
+    Genus_id int,
+    Foreign KEY (Genus_id) REFERENCES packed_animals (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO horses (Name, Birthday, Commands, Genus_id)
+VALUES ('Яблоко', '2020-01-12', 'бегом, шагом', 1),
+('Зак', '2017-03-12', "бегом, шагом, хоп", 1),  
+('Беляк', '2016-07-12', "бегом, шагом, хоп, брр", 1), 
+('Молния', '2020-11-10', "бегом, шагом, хоп", 1);
+
+CREATE TABLE donkeys 
+(       
+    Id INT AUTO_INCREMENT PRIMARY KEY, 
+    Name VARCHAR(20), 
+    Birthday DATE,
+    Commands VARCHAR(50),
+    Genus_id int,
+    Foreign KEY (Genus_id) REFERENCES packed_animals (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO donkeys (Name, Birthday, Commands, Genus_id)
+VALUES ('Первый', '2019-04-10', NULL, 2),
+('Второй', '2020-03-12', "", 2),  
+('Третий', '2021-07-12', "", 2), 
+('Четвертый', '2022-12-10', NULL, 2);
+
+CREATE TABLE camels 
+(       
+    Id INT AUTO_INCREMENT PRIMARY KEY, 
+    Name VARCHAR(20), 
+    Birthday DATE,
+    Commands VARCHAR(50),
+    Genus_id int,
+    Foreign KEY (Genus_id) REFERENCES packed_animals (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO camels (Name, Birthday, Commands, Genus_id)
+VALUES ('Горбатый', '2022-04-10', 'вернись', 3),
+('Двугорб', '2019-03-12', "остановись", 3),  
+('Сон', '2015-07-12', "повернись", 3), 
+('Род', '2022-12-10', "улыбнись", 3);
